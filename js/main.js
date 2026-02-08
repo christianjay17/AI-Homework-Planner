1a0169aaedebed0119aaa165ee2b685b336b31c1
christianjay17 <christianschrader12@gmail.com>
2026-02-07 19:45:26 -0800

    wired up first name and history to get looged in console

--- js/main.js
+++ js/main.js
@@ -1,15 +1,40 @@
 function initApp() {
-    console.log("hi")
-     const myButton = document.getElementById("submitBtn")
-    const input = document.getElementById("first_name")
-    const checkboxHistory = document.getElementById("subject-history")
+    const myButton = document.getElementById("submitBtn")
+    const start_time = document.getElementById("start-time")
+    const history_inputs = document.getElementById("")
+    
+    const context = {
+      
+    }
 
     myButton.addEventListener("click", () => {
-    console.dir(checkboxHistory.checked)
-      console.log("My Name:",input.value)
+      const checkboxHistory = document.getElementById("subject-history")
+      const first_name = document.getElementById("first_name")
+      const history_time = document.getElementById("subject-history-time")
+      
+console.log(checkboxHistory.value)
+      if (first_name.value) {
+        context.firstName = first_name.value
+        console.log("First name IS set:", context.firstName)
+      } else {
+        console.log("First name not set", context)
+      }
+
+      if (checkboxHistory.checked == true) {
+        context.history = history_time.value
+        console.log("History IS Checked", checkboxHistory.checked)
+      } else {
+        console.log("History IS NOT Checked", context)
+      }
+      console.log(context)
     })
 
+
+    
 }
 
 initApp()
-// document.addEventListener("DOMContentLoaded", initApp);
+
+
+
+// document.addEventListener("DOMContentLoaded", initApp); 
